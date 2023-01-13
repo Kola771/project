5b82ce421feea378b562955cd808fa69c605c403
kola771 <koladeaboudou@gmail.com>
2023-01-13 21:18:10 +0100

    update 13/01/2023 21h18

--- App/freek.sql
+++ App/freek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mer. 11 jan. 2023 à 18:21
+-- Généré le : ven. 13 jan. 2023 à 20:16
 -- Version du serveur : 5.7.33
 -- Version de PHP : 8.1.12
 
@@ -33,22 +33,18 @@ CREATE TABLE `books` (
   `book_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `book_description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `book_status` enum('En ligne','En attente','Retirer') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` date NOT NULL,
-  `updated_at` date DEFAULT NULL
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `books`
 --
 
-INSERT INTO `books` (`book_id`, `book_name`, `book_image`, `book_description`, `book_status`, `created_at`, `updated_at`) VALUES
-('comics-ok', 'Hhh', 'book-image-63bec87f5ebcb2.05544558.jpg', 'jjjjn dfgffn gdfg', 'En ligne', '2023-11-01', NULL),
-('mangas-belz', 'belzebub', 'book-image-63beb6dc1355e5.79279609.jpg', '1- Lorem 2- Lorem 3- Lorem ', 'En ligne', '2023-11-01', NULL),
-('mangas-Blc', 'bleach', 'book-image-63beb675f1ac31.01086370.jpg', 'DevDocs combines multiple API documentations in a fast, organized, and searchable interface. Here&#039;s what you should know before you start: Open the Preferences to enable more docs and customize the UI. You don&#039;t have to use your mouse — see the list of keyboard shortcuts. The search supports fuzzy matching (e.g. &quot;bgcp&quot; brings up &quot;background-clip&quot;). To search a specific documentation, type its name (or an abbr.), then Tab. You can search using your browser&#039;s address bar — learn how. DevDocs works offline, on mobile, and can be installed as web app. For the latest news, follow @DevDocs. DevDocs is free and open source. And if you&#039;re new to coding, check out freeCodeCamp&#039;s open source curriculum. ', 'En ligne', '2023-11-01', NULL),
-('mangas-Exi', 'Existence', 'book-image-63bc62ad6d9ef7.75524935.jpg', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam quaerat dolorem doloribus adipisci perspiciatis non. Laborum, cum vero commodi corporis et nulla consectetur magni, ratione, dolore quod perspiciatis nihil. Maiores!', 'En ligne', '2023-09-01', NULL),
-('mangas-KNY', 'Demon Slayer', 'book-image-63bec5c772e604.15723956.jpg', 'il était une fois... ', 'En ligne', '2023-11-01', NULL),
-('mangas-PFTO', 'Player from today onward', 'book-image-63bc62d4d76b22.19812603.jpg', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam quaerat dolorem doloribus adipisci perspiciatis non. Laborum, cum vero commodi corporis et nulla consectetur magni, ratione, dolore quod perspiciatis nihil. Maiores!', 'En ligne', '2023-09-01', NULL),
-('mangas-WDD', 'White Dragon Duke: Pendragon', 'book-image-63bc6216e1f6f1.53441000.jpg', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam quaerat dolorem doloribus adipisci perspiciatis non. Laborum, cum vero commodi corporis et nulla consectetur magni, ratione, dolore quod perspiciatis nihil. Maiores!', 'En ligne', '2023-09-01', NULL);
+INSERT INTO `books` (`book_id`, `book_name`, `book_image`, `book_description`, `book_status`, `created_at`) VALUES
+('mangas-Exi', 'Existence', 'book-image-63c140cc711404.62233725.jpg', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis delectus iste sint ducimus necessitatibus deserunt rerum sapiente dolor suscipit ad,<br />\r\ncumque dolorem pariatur voluptatum quas nobis temporibus <br />\r\nlibero dolore consequuntur?', 'En ligne', '2023-01-13 16:47:31'),
+('mangas-PFTO', 'Player From Today Onward', 'book-image-63c141443d76a5.79108854.jpg', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis delectus iste sint ducimus <br />\r\n<br />\r\nnecessitatibus deserunt rerum sapiente dolor suscipit ad, <br />\r\n<br />\r\ncumque dolorem pariatur voluptatum quas nobis temporibus libero dolore consequuntur?', 'Retirer', '2023-01-13 19:11:27'),
+('mangas-rai', 'Reincarnation', 'book-image-63c14177bb2a65.65573137.jpg', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis delectus iste sint ducimus <br />\r\nnecessitatibus deserunt rerum sapiente dolor suscipit ad, <br />\r\ncumque dolorem pariatur voluptatum quas nobis temporibus libero dolore consequuntur?', 'En ligne', '2023-01-13 19:11:29'),
+('mangas-WDD', 'White Dragon Duke: Pendragon', 'book-image-63c13ceb28b1e2.04535772.jpg', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam, dolores at excepturi omnis incidunt temporibus sequi eligendi ratione ullam eum,<br />\r\n<br />\r\nsit ipsa impedit obcaecati saepe magnam cupiditate delectus cumque dolorum.', 'En ligne', '2023-01-13 16:53:41');
 
 -- --------------------------------------------------------
 
@@ -78,19 +74,20 @@ CREATE TABLE `comments` (
   `comment_comment` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` int(11) NOT NULL,
   `book_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` date NOT NULL,
-  `updated_at` date DEFAULT NULL
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `comments`
 --
 
-INSERT INTO `comments` (`comment_id`, `comment_comment`, `user_id`, `book_id`, `created_at`, `updated_at`) VALUES
-(1, 'Cool ce manga !!', 11, 'mangas-Blc', '2023-11-01', NULL),
-(2, 'Nul ce manga !!!', 11, 'mangas-Blc', '2023-11-01', NULL),
-(3, 'Interessant ce mangas', 11, 'mangas-Blc', '2023-11-01', NULL),
-(4, 'Cool comme manga !!!', 12, 'mangas-Blc', '2023-11-01', NULL);
+INSERT INTO `comments` (`comment_id`, `comment_comment`, `user_id`, `book_id`, `created_at`) VALUES
+(5, 'Bienvenu(e) chers lecteurs!!!<br />\r\nJ&#039;espère que ce manhua vous l&#039;aimeriez car elle regorge beaucoup de retranchement.', 2, 'mangas-Exi', '2023-01-13 09:41:10'),
+(6, 'Super ce mangas !!!<br />\r\n<br />\r\nLisez-le les gars !!!', 2, 'mangas-rai', '2023-01-13 10:34:08'),
+(7, 'J&#039;ai vraiment aimé le scénario...', 4, 'mangas-rai', '2023-01-13 10:37:04'),
+(8, 'Ah... encore ce manga !!!', 4, 'mangas-PFTO', '2023-01-13 10:42:54'),
+(10, 'Laisser un commentaire. <br />\r\n<br />\r\nJe te dis QUE TU ES UN GARS BON !!!', 4, 'mangas-WDD', '2023-01-13 05:21:29'),
+(11, 'C&#039;est trop ouf !!!', 3, 'mangas-WDD', '2023-01-13 05:42:23');
 
 -- --------------------------------------------------------
 
@@ -100,28 +97,25 @@ INSERT INTO `comments` (`comment_id`, `comment_comment`, `user_id`, `book_id`, `
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
-  `user_firstname` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_lastname` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_username` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_email` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_wordkey` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_role` enum('0','1') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` date NOT NULL,
-  `updated_at` date DEFAULT NULL
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`user_id`, `user_firstname`, `user_lastname`, `user_username`, `user_email`, `user_password`, `user_image`, `user_wordkey`, `user_role`, `created_at`, `updated_at`) VALUES
-(11, 'Koladé Moussè', 'ABOUDOU', 'As2Pic444', 'kola444@gmail.com', '$2y$10$BIFVnUq3YXxLmb3guI87DuyL3dGrPDMF4jCNv7i8jdCzoOL4bsupy', 'img-63bc36289b3899.64020701.jpg', 'asdePic771am', '1', '2023-06-01', '2023-09-01'),
-(12, 'Amour', 'DAHOU', 'Dahounassara021', 'NassAmour@gmail.com', '$2y$10$eHBUXsjNOr4DbPLGJrMFcOuEAkBRUynsLJ5sx7ky3M5pgKZbg01DC', 'img-63bc3600a2ca63.00050143.jpg', 'Motcle@2001A', '1', '2023-07-01', '2023-09-01'),
-(13, 'Hilary', 'ZOGO', 'Lary10', 'word@gmail.com', '$2y$10$4KU/munqXb5FSL5XXfxKheQZ8IKI79vZvu.dLVM2moBvh.7t9VFiK', NULL, 'jwdfjklbefjk', '1', '2023-07-01', NULL),
-(14, 'Gdgtgh', 'HBDGHDGH', 'F12@50', 'hh@gmail.com', '$2y$10$IleIoLSlBNRRbwweGMf9UuFmFKcLLzHAgBHVqUEMsAI7j4mqFQqfS', NULL, 'aaaaaaaaaaaa', '1', '2023-07-01', NULL),
-(15, 'Brunice', 'PADONOU', 'Thelo007', 'brunicepdn@gmail.com', '$2y$10$2LlMa1H2wEl4VGK4Ynk5Q.gawAHIHqZ/mUBnwm4YKEwV9YEn4ScG6', NULL, 'thelo007brun', '1', '2023-09-01', NULL);
+INSERT INTO `users` (`user_id`, `user_firstname`, `user_lastname`, `user_username`, `user_email`, `user_password`, `user_image`, `user_wordkey`, `user_role`, `created_at`) VALUES
+(2, 'Koladé Moussè', 'ABOUDOU', 'AsdePic444', 'kola444@gmail.com', '$2y$10$D7.SUQgqerryoYRuCmOvsurT.QHbnUn7KQvtoh3dwdFfOyPbGHiQW', 'img-63c1219dbf29e3.37811370.jpg', 'asdepic444', '0', '2023-01-13 11:44:07'),
+(3, 'Nassara Amour', 'DAHOU', 'Dahounassara021', 'NassAmour@gmail.com', '$2y$10$bqLYT0c.tpCpZMESSmGVR.Xmn3uVMg6hG5fN0NBw2CqOfFzfIzDFm', 'img-63c123aa8c4674.36008937.jpg', 'aaaaaaaaaaaa', '1', '2023-01-13 09:26:02'),
+(4, 'Donald', 'AHOSSI', 'Doni-yen001', 'ahossi@gmail.com', '$2y$10$wQgxhXFQ9iPbr7nZiNYowevVWvMfVi9t9BVSTouarHSH3mw/Mquau', 'img-63c14238458801.29282201.jpg', 'donidoniyen', '1', '2023-01-13 11:36:24');
 
 --
 -- Index pour les tables déchargées
@@ -172,13 +166,13 @@ ALTER TABLE `chapters`
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
